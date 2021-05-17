cf7cbc8473eb2592e294a51bc9f9780e421646da
deepakyadav <deepakyadav21info@gmail.com>
2021-05-18 02:37:25 +0530

    Built calculator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  
+  let [expression, setExpression] = useState("");
+  let [oldExpression, setOldExpression] = useState("");
+  let [prev, setPrev] = useState("ANS");
+  
+  let numerics = new Set("0123456789.");
+  let operators = new Set("+-*/%");
+
+  let buttons = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"];
+  let evaluateExpression = function(){
+      let evaluation = eval(expression);
+      setOldExpression(expression + "=");
+      setExpression(String(evaluation));
+      setPrev("ANS");
+  };
+  let putNumerics = function(value){
+    if(prev === "ANS"){
+      setOldExpression("Ans = "+ expression)
+      setExpression(value);
+    }
+    else{ setExpression(expression + value); }
+    setPrev("NUM");
+  }
+  let putOperator = function(value){
+    if(prev !== "OP"){
+      setExpression(expression + value);
+    }
+    else {
+      setExpression(expression.slice(0,-1) + value);
+    }
+    setPrev("OP");
+  }
+
+  let handleKeyUp = function(event){
+    console.log(event.key);
+    if(event.key === "Backspace" && expression.length >= 1){
+      setExpression(expression.slice(0,-1));
+      setPrev("DEL");
+    }
+    else if(numerics.has(event.key)){
+      putNumerics(event.key); }
+    else if(operators.has(event.key)){
+      putOperator(event.key);
+    }
+    else if(event.key === "Enter"){
+      evaluateExpression();
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" tabIndex={0} onKeyUp={handleKeyUp} style={{
+      width : "100%",
+      height : "100vh",
+      background : "#444444",
+      display : "flex",
+      flexDirection : "column",
+      justifyContent : "center",
+      alignItems : "center",
+    }}>
+      <div style={{
+      width : "450px",
+      height: "70px",
+      background : "#FF5E13",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: "0px 20px",
+      borderRadius: "10px",
+      overflow: "hidden"
+      }}>
+        <p style={{
+          fontSize: "40px",
+          color: "#ffffff",
+          fontWeight: "bold"
+        }}>CALCULATOR</p>
+      </div>
+      <div style={{
+      width : "450px",
+      background : "#ffffff",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-end",
+      justifyContent: "center",
+      padding: "20px",
+      marginTop: "20px",
+      marginBottom: "20px",
+      borderRadius: "10px",
+      overflow: "hidden"
+      }}>
+        <h3>{oldExpression}</h3>
+        <h1>{expression}</h1>
+      </div>
+
+      <div style={{
+      width : "450px",
+      background : "#ffffff",
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      alignItems: "flex-end",
+      justifyContent: "center",
+      padding: "20px",
+      borderRadius: "10px"
+      }}>
+        {buttons.map(function(buttonValue,index){
+          return <button style={{
+            fontSize: "18px",
+            width: "90px",
+            margin:"5px 10px",
+            padding: "4px 7px",
+            background: "#9999",
+            fontWeight: "bold",
+            borderRadius: "5px"
+          }} onClick={function(){
+            if(buttonValue === "="){
+              evaluateExpression();
+            }
+            else if(buttonValue === "AC" && expression.length >= 1){
+              setExpression(expression.slice(0,-1));
+              setPrev("DEL");
+            }
+            else if(operators.has(buttonValue)){
+              putOperator(buttonValue);
+            }
+            else if(numerics.has(buttonValue)){
+              putNumerics(buttonValue);
+            }
+          }}>{buttonValue}</button>
+        })}
+      </div>
+
     </div>
   );
 }
